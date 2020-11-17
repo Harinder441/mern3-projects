91e59d8554d8f5a5f56e78763c533f0823a2399a
Nabhan <nabhan@criodo.com>
2020-11-17 09:53:14 +0530

    [setup.sh] update setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+rm -rf ~/node_modules
+rm -rf node_modules
+rm -rf ~/.npm 
+rm package-lock.json
 mkdir -p ~/node_modules
 ln -s /home/crio-user/node_modules /home/crio-user/workspace/bytes/me_react_basics1/node_modules
 npm install
